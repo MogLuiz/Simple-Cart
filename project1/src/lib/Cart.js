10685a747fb478343152182b6590355a06f92fa2
Luiz Henrique <luizhjramos@outlook.com>
2022-04-04 16:31:57 -0300

    feat: getTotal method implement

--- project1/src/lib/Cart.js
+++ project1/src/lib/Cart.js
@@ -1,7 +1,5 @@
-describe('Cart', () => {
-  it('should return 0 when getTotal() is executed in a newly created instance', () => {
-    const cart = new Cart();
-
-    expect(cart.getTotal()).toEqual(0);
-  });
-});
+export default class Cart {
+  getTotal() {
+    return 0;
+  }
+}
